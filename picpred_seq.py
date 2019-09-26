799e8cafe6ce3b5a654cc278856b792108f2f20c
patrickbryant1 <patrick.bryant@live.com>
2019-09-26 09:50:56 +0200

    rewriting the net to fit the new allele sequence encodings

--- picpred_seq.py
+++ picpred_seq.py
@@ -42,10 +42,18 @@ parser.add_argument('train_df', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to df.')
 parser.add_argument('train_aa_enc', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to file with amino acid encodings')
+parser.add_argument('train_C1', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to file with allele encodings for chain 1.')
+parser.add_argument('train_C2', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to file with allele encodings for chain 2.')
 parser.add_argument('test_df', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to df.')
 parser.add_argument('test_aa_enc', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to file with amino acid encodings')
+parser.add_argument('test_C1', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to file with allele encodings for chain 1.')
+parser.add_argument('test_C2', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to file with allele encodings for chain 2.')
 parser.add_argument('allele_enc_path', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to directory with allele encodings')
 #parser.add_argument('params_file', nargs=1, type= str,
@@ -89,72 +97,63 @@ def pad(ar, x, y):
 
     return ar
 
-def encode_alleles(alleles, allele_embs):
-    '''Encodes all alleles in accordance to the autoencodings in allele_embs
-    '''
-
-    allele_encodings = [] #Save allele encodings
-    for al in alleles:
-        al = al.split('/')
-        if len(al) > 1:
-            enc = np.concatenate((allele_embs[al[0]],allele_embs[al[1]]))
-        else:
-            enc = np.concatenate((allele_embs[al[0]],allele_embs[al[0]]))
-        allele_encodings.append(enc)
-
-    return allele_encodings
 
 #MAIN
 args = parser.parse_args()
-df_path = args.dataframe[0]
-aa_enc = np.load(args.aa_encodings[0], allow_pickle = True)
-allele_enc_path = args.allele_enc_path[0]
+train_df = pd.read_csv(args.train_df[0])
+train_aa_enc = np.load(args.train_aa_enc[0], allow_pickle = True)
+train_c1 = np.load(args.train_C1[0], allow_pickle = True)
+train_c2 = np.load(args.train_C2[0], allow_pickle = True)
+test_df = pd.read_csv(args.test_df[0])
+test_aa_enc = np.load(args.test_aa_enc[0], allow_pickle = True)
+test_c1 = np.load(args.test_C1[0], allow_pickle = True)
+test_c2 = np.load(args.test_C2[0], allow_pickle = True)
 #params_file = args.params_file[0]
 out_dir = args.out_dir[0]
 
-#Assign data and labels
-#Read df
-df = pd.read_csv(df_path)
-#Get pic50 values
-pic50 = -np.log10(df['measurement_value'])
-#bins = np.array([-5.47712125, -4.77712125, -4.07712125, -3.37712125, -2.69897,
-#       -1.97712125, -1.27712125, -0.57712125,  0.12287875,  0.82287875])
-bins = np.array([-5.48, -5.13, -4.78, -4.43, -4.08, -3.73, -3.38, -3.03, -2.68,
-       -2.33, -1.98, -1.63, -1.28, -0.93, -0.58, -0.23,  0.12,  0.47,
-        0.82])
-#Bin the pic50 values
-y_binned = np.digitize(pic50,bins)
-y = pic50
-X1 =[]
+#bins
+bins = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+
+#Get labels
+y_train = np.asarray(train_df['log50k'])
+y_test = np.asarray(test_df['log50k'])
+#Get y_test in binary
+#When classifying the peptides into binders and non-binders
+#for calculation of the AUC values for instance, a threshold of 500 nM is used.
+#This means that peptides with log50k transformed binding
+#affinity values greater than 0.426 are classified as binders.
+
+t = 1-np.log(500)/np.log(50000)
+true_binary = []
+for i in range(len(y_test)):
+    if y_test[i]>=t:
+        true_binary.append(1)
+    else:
+        true_binary.append(0)
+
 #onehot encode aa_enc
-for i in range(len(aa_enc)):
-    X1.append(np.eye(20)[aa_enc[i]])
-max_length = 25 #Goes from 15-25
+#train
+X1_train =[]
+for i in range(len(train_aa_enc)):
+    X1_train.append(np.eye(20)[train_aa_enc[i]])
+max_length = 37 #Goes from 9-37
+for i in range(len(X1_train)):
+    X1_train[i] = pad(X1_train[i], max_length, 20)
+X1_train = np.array(X1_train)
+#test
+X1_test =[]
+for i in range(len(test_aa_enc)):
+    X1_test.append(np.eye(20)[test_aa_enc[i]])
+max_length = 37 #Goes from 9-37
+for i in range(len(X1_test)):
+    X1_test[i] = pad(X1_test[i], max_length, 20)
+X1_test = np.array(X1_test)
 
-for i in range(len(X1)):
-    X1[i] = pad(X1[i], max_length, 20)
-
-X1 = np.array(X1)
-
-#Get allele encodings
-allele_dict = {}
-aa_enc_files = glob.glob(allele_enc_path+'*_alpha.npy')
-for file in aa_enc_files:
-    name = file.split('/')[-1].split('_')[0]
-    enc = np.load(file, allow_pickle = True)
-    enc = np.eye(21)[enc]
-    allele_dict[name] = enc
 
 #Encode alleles
-allele_encodings = encode_alleles([*df['allele']], allele_dict)
-X2 = np.asarray(allele_encodings)
-#y= y_binned
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
-for train_index, test_index in sss.split(X2, y_binned):
-    X1_train, X1_test = X1[train_index], X1[test_index]
-    X2_train, X2_test = X2[train_index], X2[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-bins = np.expand_dims(bins, axis=0)
+#Each chain of the alleles is represented by 94 aa
+for i in range(len(test_aa_enc)):
+    X1_test.append(np.eye(20)[test_aa_enc[i]])
 
 #Tensorboard for logging and visualization
 log_name = str(time.time())
